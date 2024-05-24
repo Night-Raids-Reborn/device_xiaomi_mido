ae72dc22afb31b361ac109fdf2fb0fd3cd704e36
darknius09 <darkniuslightness@gmail.com>
2024-05-24 02:11:59 +0000

    mido: add blur props

--- product_prop.mk
+++ product_prop.mk
@@ -8,3 +8,9 @@
 # Default to BFQ I/O scheduler
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.io.scheduler=bfq
+
+# Blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.sf.disable_blurs=0 \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
